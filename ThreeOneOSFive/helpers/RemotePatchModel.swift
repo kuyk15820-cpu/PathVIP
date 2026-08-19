8d4a954c5ff2ffcbf88eb8ee592f09047096328e
kuyk15820-cpu <kuyk15820@gmail.com>
2026-08-19 23:13:31 +0700

    Create RemotePatchModel.swift

---
+++ ThreeOneOSFive/helpers/RemotePatchModel.swift
@@ -0,0 +1,17 @@
+import Foundation
+
+struct RemotePatchConfig: Decodable {
+    let patchID: String
+    let projectName: String
+    let targetBundleID: String
+    let targetPath: String
+    let fileURL: URL
+    
+    enum CodingKeys: String, CodingKey {
+        case patchID = "patch_id"
+        case projectName = "project_name"
+        case targetBundleID = "target_bundle_id"
+        case targetPath = "target_path"
+        case fileURL = "file_url"
+    }
+}
